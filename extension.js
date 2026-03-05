9ef98375b4021cae3a1c0607a4a8f8d3f632987f
macminim2 <mynightgirl@noreply.gitcode.com>
2026-03-06 00:49:24 +0800

    v0.0.7   bug about jszip fix

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require("vscode");
 
-const { GgbTreeDataProvider, openFile } = require("./ggbTreeDataProvider");
+// const { GgbTreeDataProvider, openFile } = require("./ggbTreeDataProvider");
 const {GgbEditorProvider} = require("./editorProvider");
 const log = vscode.window.createOutputChannel("GGB View");
 
@@ -10,13 +10,13 @@ const log = vscode.window.createOutputChannel("GGB View");
 function activate(context) {
   log.appendLine('Congratulations, your extension "ggbview" is now active!');
 
-  const pickggb = vscode.commands.registerCommand("ggbview.pickFile", (uri) => {
-    if (uri && (uri.fsPath.endsWith(".ggb") || uri.fsPath.endsWith(".ggt"))) {
-      vscode.window.showInformationMessage(`Pick GGB file: ${uri.fsPath}`);
-      treeDataProvider.setUri(uri);
-    }
-  });
-  context.subscriptions.push(pickggb);
+  // const pickggb = vscode.commands.registerCommand("ggbview.pickFile", (uri) => {
+  //   if (uri && (uri.fsPath.endsWith(".ggb") || uri.fsPath.endsWith(".ggt"))) {
+  //     vscode.window.showInformationMessage(`Pick GGB file: ${uri.fsPath}`);
+  //     treeDataProvider.setUri(uri);
+  //   }
+  // });
+  // context.subscriptions.push(pickggb);
 
   const opendefault = vscode.window.registerCustomEditorProvider(
     "ggbview.ggbView",
@@ -34,21 +34,21 @@ function activate(context) {
   });
   context.subscriptions.push(opencmd);
 
-  const treeDataProvider = new GgbTreeDataProvider();
-  const explorer_tree = vscode.window.createTreeView("ggbfilelist", {
-    treeDataProvider: treeDataProvider,
-  });
+  // const treeDataProvider = new GgbTreeDataProvider();
+  // const explorer_tree = vscode.window.createTreeView("ggbfilelist", {
+  //   treeDataProvider: treeDataProvider,
+  // });
 
-  context.subscriptions.push(explorer_tree);
+  // context.subscriptions.push(explorer_tree);
 
-  const openggbsubfile = vscode.commands.registerCommand(
-    "ggbview.openFile",
-    (ggbUri, filePath) => {
-      openFile(ggbUri, filePath);
-      // vscode.window.showInformationMessage(`${filePath}`);
-    }
-  );
-  context.subscriptions.push(openggbsubfile);
+  // const openggbsubfile = vscode.commands.registerCommand(
+  //   "ggbview.openFile",
+  //   (ggbUri, filePath) => {
+  //     openFile(ggbUri, filePath);
+  //     // vscode.window.showInformationMessage(`${filePath}`);
+  //   }
+  // );
+  // context.subscriptions.push(openggbsubfile);
 }
 
 function deactivate() {}
@@ -62,9 +62,12 @@ module.exports = {
 ## 关于发布更新
 
 - 发布前需要先获取azure的token，[azure-token管理](https://dev.azure.com/wchenchen/_usersSettings/tokens?)
-- [安装量查看](https://marketplace.visualstudio.com/manage/publishers/neon-light)
+- [安装量查看、更新插件](https://marketplace.visualstudio.com/manage/publishers/neon-light)
 
 ## 插件开发文档
+
+新版本使用命令 `vsce package` 打包生成 ggbview-0.0.7.vsix 包
+使用 `vsce publish` 发布插件
 
 - [插件开发文档](https://code.visualstudio.com/api/extension-guides/custom-editors#custom-editor)
 
